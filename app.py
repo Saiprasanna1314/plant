da06d74a264a44656673b46383729483ed5fdbd1
Saiprasanna1314 <saiprasannachekuri@gmail.com>
2025-12-22 16:25:47 +0530

    Fix labels, prevent crashes, clean app

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ labels = [
     "Blueberry___healthy",
     "Cherry_(including_sour)___Powdery_mildew",
     "Cherry_(including_sour)___healthy",
-    "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
+    "Corn_(maize)___Cercospora_leaf_spot_Gray_leaf_spot",
     "Corn_(maize)___Common_rust_",
     "Corn_(maize)___Northern_Leaf_Blight",
     "Corn_(maize)___healthy",
@@ -59,12 +59,13 @@ labels = [
     "Tomato___Late_blight",
     "Tomato___Leaf_Mold",
     "Tomato___Septoria_leaf_spot",
-    "Tomato___Spider_mites Two-spotted_spider_mite",
+    "Tomato___Spider_mites_Two-spotted_spider_mite",
     "Tomato___Target_Spot",
     "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
     "Tomato___Tomato_mosaic_virus",
     "Tomato___healthy"
 ]
+
 
 # ---------------- PLANT CHECK ----------------
 def is_plant_image(img_array):
